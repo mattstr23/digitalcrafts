56ff264afd28d8570e465234cfdccf0e24c18aba
Matthew Valicoff <matthewvalicoff@Matthews-MacBook-Pro.local>
2021-08-30 01:42:47 -0500

    Movie Assignment Completion

--- week4/Day3/MoviePage/movie.js
+++ week4/Day3/MoviePage/movie.js
@@ -1,15 +1,26 @@
 const apiKey = "8644e34e";
 const movieDB = `http://www.omdbapi.com/?apikey=${apiKey}`;
 
-const plaqContainer = document.querySelector(".moviePlaq");
+const movieContainer = document.querySelector(".movieContainer");
 const searchButton = document.querySelector(".searchButton");
 
 async function moviePull() {
-    plaqContainer.innerHTML = "";
-    const input = document.querySelector(".search");
+    movieContainer.innerHTML = "";
+    const input = document.querySelector(".search").value;
     const fetchMovie = await fetch(movieDB + `&s=${input}`);
     const jsonMovie = await fetchMovie.json();
-    console.table(jsonMovie)
 
-
+    for (const movie of jsonMovie.Search) {
+        const movieTitle = document.createElement("h4");
+        const movieYear = document.createElement("h5")
+        const moviePoster = document.createElement("img");
+        const movieDiv = document.createElement("div");
+        movieDiv.className = "moviePlaq"
+        movieTitle.innerHTML = movie.Title;
+        moviePoster.src = movie.Poster;
+        movieYear.innerHTML = movie.Year;
+        movieDiv.append(movieTitle, moviePoster, movieYear);
+        movieContainer.append(movieDiv);
+    }
 }
+searchButton.addEventListener("click", () => moviePull())
