64cc3a2243b378a297b75b9d71ea90165ea60014
Matthew Valicoff <matthewvalicoff@Matthews-MacBook-Pro.local>
2021-09-01 15:28:11 -0500

    project push

--- week5/Project/nutrition.js
+++ week5/Project/nutrition.js
@@ -7,21 +7,25 @@ async function pullRecipe() {
     const jsonRecipe = await fetchrecipe.json();
     
     for (let recipe of jsonRecipe.results) {
+
         const recipeName = document.createElement("h3");
         const recipePic = document.createElement("img");
         const recipeSummary = document.createElement("p");
-        // const recipeUrl = document.createElement("a");
-
+        const recipeUrl = document.createElement("a");
+        console.log(recipe)
         recipeName.innerHTML = recipe.title;
         recipePic.src = recipe.image;
         recipeSummary.innerHTML = recipe.summary;
-        // recipeUrl.href = recipe.spoonacularSourceUrl;
-        
-        foodContainer.append(recipeName, recipePic, recipeSummary)
+        recipeUrl.href = recipe.spoonacularSourceUrl;
+        recipeUrl.innerText = "Recipe Link"
+        console.log(recipe.spoonacularSourceUrl)
+        foodContainer.append(recipeName, recipePic, recipeSummary, recipeUrl)
         
 
     }
 }
+pullRecipe()
+
 const toggleMen = document.querySelector(".toggleMenu")
 const navLinks = document.querySelector(".navLinks")
 
